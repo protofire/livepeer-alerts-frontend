f61997a417e754d0810c3cc6df2f2e546b741d41
Agustin Pane <agustin.pane@gmail.com>
2018-12-19 17:16:07 -0300

    Check if web3 is available

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import web3 from '../../web3';
 
 export class HomeComponent extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
 
-  render() {
+
+    componentDidMount() {
+    }
+
+
+    render() {
+
+
+        let content =
+        <>
+            <p>Please install metamask and come back!</p>
+            <h3>Hello LivePeer Alerts!</h3>
+        </>;
+
+        if(web3){
+            content =
+            <>
+                <h3>Hello LivePeer Alerts!</h3>
+                <p>Descriptions and details</p>
+                <Link to="/account">
+                    <button>Get Started</button>
+                </Link>
+            </>;
+        }
+
     return (
       <div>
-        <h3>Hello LivePeer Alerts!</h3>
-        <p>Descriptions and details</p>
-        <Link to="/account">
-          <button>Get Started</button>
-        </Link>
+          {content}
       </div>
     )
   }
---
+++ src/web3.js
@@ -0,0 +1,15 @@
+import Web3 from 'web3';
+
+let web3;
+
+/** We check if metamask is installed **/
+if(window.web3) {
+    /** We get the provider of web3 injected by metamask **/
+    web3 = new Web3(window.web3.currentProvider);
+} else{
+    /** The user is not running metamask, we should send a message **/
+    /** TODO **/
+}
+
+
+export default web3;
