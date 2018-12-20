a355c00b1f5de4d1eb6fd7e55824b87422060679
Agustin Pane <agustin.pane@gmail.com>
2018-12-20 09:33:33 -0300

    Commit test

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,38 +1,29 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import web3 from '../../web3';
+import web3 from '../../web3'
 
 export class HomeComponent extends Component {
+  componentDidMount() {}
 
-
-    componentDidMount() {
+  render() {
+    let content = (
+      <>
+        <p>Please install metamask and come back!</p>
+        <h3>Hello LivePeer Alerts!</h3>
+      </>
+    )
+    if (web3) {
+      content = (
+        <>
+          <h3>Hello LivePeer Alerts!</h3>
+          <p>Descriptions and details</p>
+          <Link to="/account">
+            <button>Get Started</button>
+          </Link>
+        </>
+      )
     }
 
-
-    render() {
-
-
-        let content =
-        <>
-            <p>Please install metamask and come back!</p>
-            <h3>Hello LivePeer Alerts!</h3>
-        </>;
-
-        if(web3){
-            content =
-            <>
-                <h3>Hello LivePeer Alerts!</h3>
-                <p>Descriptions and details</p>
-                <Link to="/account">
-                    <button>Get Started</button>
-                </Link>
-            </>;
-        }
-
-    return (
-      <div>
-          {content}
-      </div>
-    )
+    return <div>{content}</div>
   }
 }
--- src/web3.js
+++ src/web3.js
@@ -1,15 +1,14 @@
-import Web3 from 'web3';
+import Web3 from 'web3'
 
-let web3;
+let web3
 
 /** We check if metamask is installed **/
-if(window.web3) {
-    /** We get the provider of web3 injected by metamask **/
-    web3 = new Web3(window.web3.currentProvider);
-} else{
-    /** The user is not running metamask, we should send a message **/
-    /** TODO **/
+if (window.web3) {
+  /** We get the provider of web3 injected by metamask **/
+  web3 = new Web3(window.web3.currentProvider)
+} else {
+  /** The user is not running metamask, we should send a message **/
+  /** TODO **/
 }
 
-
-export default web3;
+export default web3
