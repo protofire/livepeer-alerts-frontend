5b00e4ac6727042f51924cf3e420fba25420f781
agupane <agustin.pane@gmail.com>
2018-12-27 08:33:49 -0300

    Removed comments on accountSummary and app.spec.js Removed old test

--- src/App.spec.js
+++ src/App.spec.js
@@ -4,13 +4,6 @@ import { configure, mount, shallow } from 'enzyme'
 import App from '../src/App'
 import Adapter from 'enzyme-adapter-react-16/build'
 import { AccountSummaryComponent, HomeComponent } from './Components'
-import spinner from './Components/Common/UI/Spinner/Spinner'
-
-/*it('renders welcome message', () => {
-  const wrapper = mount(<App />)
-  const welcome = 'Hello LivePeer Alerts!'
-  expect(wrapper.contains(welcome)).toEqual(true)
-})*/
 
 configure({ adapter: new Adapter() })
 
@@ -43,22 +36,5 @@ describe('Check public and protected routes', () => {
     expect(wrapper.find(AccountSummaryComponent)).toHaveLength(0)
     expect(wrapper.find(HomeComponent)).toHaveLength(1)
   })
-  /*  it('Expect route / to be AccountSummaryComponent if the user is authenticated', () => {
-    let props = {
-      web3: [],
-      userData: {
-        authenticated: true
-      },
-      render: false
-    }
-    const wrapper = mount(
-      <MemoryRouter initialEntries={['/account']} initialIndex={0}>
-        <App />
-      </MemoryRouter>
-    )
 
-    //expect(wrapper.find(HomeComponent)).toHaveLength(1)
-    expect.any(wrapper.find(AccountSummaryComponent)).toHaveLength(1)
-
-  })*/
 })
--- src/Components/AccountSummary/accountSummary.js
+++ src/Components/AccountSummary/accountSummary.js
@@ -4,11 +4,6 @@ export class AccountSummaryComponent extends Component {
   render() {
     const web3 = this.props
     console.log('[AccountSummaryComponent]', web3.eth)
-    /*
-    let accounts = web3.eth.getAccounts().then(accounts => {
-      console.log("accounts: ", accounts);
-    });
-*/
 
     return (
       <div>
