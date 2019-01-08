7edfeea770efec9b55c0576f8a802e162aa6a3e4
agupane <agustin.pane@gmail.com>
2019-01-08 10:24:24 -0300

    Changed homecard text, moved getStarted button

--- src/Components/Home/HomeCard/HomeCard.js
+++ src/Components/Home/HomeCard/HomeCard.js
@@ -10,14 +10,14 @@ import Code from '@material-ui/icons/Code'
 import Group from '@material-ui/icons/Group'
 
 // core components
-import GridContainer from '../Common/Grid/GridContainer.js'
-import GridItem from '../Common/Grid/GridItem.js'
-import Button from '../Common/CustomButtons/Button.js'
-import InfoArea from '../Common/InfoArea/InfoArea.js'
-import Card from '../Common/Card/Card.js'
-import CardBody from '../Common/Card/CardBody.js'
+import GridContainer from '../../Common/Grid/GridContainer.js'
+import GridItem from '../../Common/Grid/GridItem.js'
+import Button from '../../Common/CustomButtons/Button.js'
+import InfoArea from '../../Common/InfoArea/InfoArea.js'
+import Card from '../../Common/Card/Card.js'
+import CardBody from '../../Common/Card/CardBody.js'
 
-import homeCardStyle from '../../assets/jss/dashboard/views/homeCardStyle'
+import homeCardStyle from '../../../assets/jss/dashboard/views/homeCardStyle'
 
 class HomeCard extends React.Component {
   render() {
@@ -27,33 +27,28 @@ class HomeCard extends React.Component {
         <GridContainer justify="center">
           <GridItem xs={12} sm={12} md={10}>
             <Card className={classes.cardSignup}>
-              <h2 className={classes.cardTitle}>Register</h2>
+              <h2 className={classes.cardTitle}>Livepeer Notifications</h2>
               <CardBody>
                 <GridContainer justify="center">
                   <GridItem xs={12} sm={12} md={5}>
                     <InfoArea
-                      title="Marketing"
-                      description="We've created the marketing campaign of the website. It was a very interesting collaboration."
+                      title="Pro-active alert notifications"
+                      description="We provide pro-active alert notifications that will help LPT token holders to be updated about how the transcoders are performing in near real time"
                       icon={Timeline}
                       iconColor="rose"
                     />
                     <InfoArea
-                      title="Fully Coded in HTML5"
-                      description="We've developed the website with HTML5 and CSS3. The client has access to the code using GitHub."
-                      icon={Code}
-                      iconColor="primary"
-                    />
-                    <InfoArea
-                      title="Built Audience"
-                      description="There is also a Fully Customizable CMS Admin Dashboard for this product."
+                      title="Account information summary"
+                      description="You can see also a summary of your account information and your livepeer balance"
                       icon={Group}
                       iconColor="info"
                     />
+                    <InfoArea title="Open source" description="" icon={Code} iconColor="primary" />
                   </GridItem>
                   <GridItem xs={12} sm={8} md={5}>
                     <div
                       className={classes.center}
-                      style={{ paddingTop: '40%', paddingBottom: '40%' }}
+                      style={{ paddingTop: '50%', paddingBottom: '40%' }}
                     >
                       <Button onClick={onClick} round color="primary" size="lg">
                         Get started
--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -4,7 +4,7 @@ import 'react-toastify/dist/ReactToastify.css'
 import './home.css'
 import logger from '../../utils'
 import { withRouter } from 'react-router-dom'
-import HomeCard from './HomeCard'
+import HomeCard from './HomeCard/HomeCard'
 
 export class HomeComponent extends Component {
   state = {
@@ -17,46 +17,6 @@ export class HomeComponent extends Component {
     logger.log('[Home.js] getStartedBtnHandler')
     this.props.history.push('/account')
   }
-
-  /*  sendToastError = toastTime => {
-    let time = 6000
-    if (toastTime) {
-      time = toastTime
-    }
-    let cause = this.props.userData.reason
-    let errorMsg
-    switch (cause) {
-      case failReasons.NO_PERMISSIONS: {
-        errorMsg = texts.NO_PERMISSIONS
-        break
-      }
-      case failReasons.NO_WEB3: {
-        errorMsg = texts.NO_WEB3
-        break
-      }
-      case failReasons.WRONG_NETWORK: {
-        errorMsg = texts.WRONG_NETWORK
-        break
-      }
-      default: {
-        errorMsg = texts.DEFAULT_ERROR
-        break
-      }
-    }
-
-    if (!toast.isActive(this.state.toastId) && this.props.render) {
-      toast.error(errorMsg, {
-        position: toast.POSITION.TOP_RIGHT,
-        progressClassName: 'Toast-progress-bar',
-        autoClose: time,
-        toastId: this.state.toastId
-      })
-      /!** TODO -- CHECK IF THERE IS ANOTHER WAY TO USE THIS, THIS IS FOR TESTING THAT THE TOAST IS CALLED **!/
-      if (this.props.toastOpenedHandlerTest) {
-        this.props.toastOpenedHandlerTest(errorMsg)
-      }
-    }
-  }*/
 
   sendToast = (toastTime, callback) => {
     let time = 2000
