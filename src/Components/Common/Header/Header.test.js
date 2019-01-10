c76eadab4629eee66545d65c6ae33cdb8aa19fce
Agustin Pane <agustin.pane@gmail.com>
2019-01-10 14:19:08 -0300

    Added header test

---
+++ src/Components/Common/Header/Header.test.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Adapter from 'enzyme-adapter-react-16'
+import { configure, shallow, mount } from 'enzyme'
+import Header from './Header'
+
+configure({ adapter: new Adapter() })
+
+const props = {
+  routes: [
+    {
+      views: {
+        path: '',
+        name: ''
+      }
+    }
+  ],
+  location: {
+    pathname: '/'
+  }
+}
+
+describe('Header test ', () => {
+  it(`Header has appBar`, () => {
+    const wrapper = mount(<Header {...props} />)
+    const input = wrapper.find('AppBar')
+    expect(input).toHaveLength(1)
+  })
+  it(`Header has toolbar`, () => {
+    const wrapper = mount(<Header {...props} />)
+    const input = wrapper.find('Toolbar')
+    expect(input).toHaveLength(1)
+  })
+  it(`Header has menu`, () => {
+    const wrapper = mount(<Header {...props} />)
+    const input = wrapper.find('Menu')
+    expect(input).toHaveLength(1)
+  })
+})
