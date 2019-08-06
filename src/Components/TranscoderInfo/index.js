ae9e59ee1f742794077e66dc894ffc916641a669
Mariano Aguero <mariano.aguero@altoros.com>
2019-08-06 11:10:01 -0300

    Add tooltip for last reward calls

--- src/Components/TranscoderInfo/index.js
+++ src/Components/TranscoderInfo/index.js
@@ -6,6 +6,7 @@ import StrippedList, { TR, TD } from '../Common/StrippedList'
 import styled from 'styled-components'
 import SmallLoadingCard from '../Common/SmallLoadingCard'
 import { truncateStringInTheMiddle, decimalPlaces } from '../../Utils'
+import Tooltip from '../Common/Tooltip'
 
 const Status = styled.h3`
   align-items: center;
@@ -45,6 +46,10 @@ const SpanPipe = styled.span`
   padding-left: 5px;
 `
 
+const TitleWithHelp = styled.div`
+  display: flex;
+`
+
 const TranscoderInfo = props => {
   const { myDelegateData, summaryData, ...restProps } = props
 
@@ -61,6 +66,17 @@ const TranscoderInfo = props => {
   const roiEvery1000 = (myDelegateData && myDelegateData.roiPercent) || 0
 
   const isActive = (summaryData && summaryData.delegator && summaryData.delegator.delegateIsActive) || true
+
+  const haveDelegateCalledReward =
+    (summaryData && summaryData.delegator && summaryData.delegator.delegateCalledReward) || true
+  let bondedDescription
+
+  if (haveDelegateCalledReward) {
+    bondedDescription = 'The delegate has successfully claimed the last inflationary token rewards.'
+  } else {
+    bondedDescription = 'Unfortunately the delegate has not claimed the last inflationary token rewards.'
+  }
+
   const delegateData = {
     address: address,
     data: [
@@ -73,7 +89,12 @@ const TranscoderInfo = props => {
         data: `${decimalPlaces(rewardCutPercentage)}%`,
       },
       {
-        title: 'Missed Reward Calls',
+        title: (
+          <TitleWithHelp>
+            Missed Reward Calls
+            <Tooltip description={bondedDescription} />
+          </TitleWithHelp>
+        ),
         data: `${missedRewardCalls}/30`,
       },
       {
@@ -121,7 +142,7 @@ const TranscoderInfo = props => {
           return (
             <TR key={index}>
               <TDTitle>{item.title}</TDTitle>
-              <TDData>{item.data}</TDData>
+              <TDData>{item.data} </TDData>
             </TR>
           )
         })}
