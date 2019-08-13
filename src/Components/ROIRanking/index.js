6c7b1df8321edab221df61ae8bb907125a3a41ae
Mariano Aguero <mariano.aguero@altoros.com>
2019-08-13 14:31:43 -0300

    Update columns and data for roi ranking

--- src/Components/ROIRanking/index.js
+++ src/Components/ROIRanking/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import Card from '../Common/Card'
-import Pagination from '../Common/Pagination'
 import PageTitle from '../Common/PageTitle'
 import Table, { TR, TH, TD } from '../Common/Table'
 import styled from 'styled-components'
@@ -76,133 +75,25 @@ const TempTitleStyled = styled.span`
 
 const data = [
   {
-    efficiency: '100%',
-    lastRound: '1378',
-    qtyRounds: '120',
     rank: '1',
-    rewardCut: '75%',
-    roiETH: '0.0821660',
-    roiPer100LPT: '3.2176',
     status: 'active',
+    address: '0x1234...7890',
     totalStake: '42184.75',
-    transcoder: '0x1234...7890',
+    rewardCut: '75%',
+    efficiency: '100%',
+    activeRounds: '120',
+    roiPer100LPT: '3.2176',
     trend: 'up',
   },
   {
-    efficiency: '100%',
-    lastRound: '1378',
-    qtyRounds: '120',
     rank: '2',
-    rewardCut: '75%',
-    roiETH: '0.0821660',
-    roiPer100LPT: '3.2176',
-    status: 'inactive',
-    totalStake: '42184.75',
-    transcoder: '0x1234...7890',
-    trend: 'down',
-  },
-  {
-    efficiency: '100%',
-    lastRound: '1378',
-    qtyRounds: '120',
-    rank: '3',
-    rewardCut: '75%',
-    roiETH: '0.0821660',
-    roiPer100LPT: '3.2176',
     status: 'active',
+    address: '0x1234...7890',
     totalStake: '42184.75',
-    transcoder: '0x1234...7890',
-    trend: 'up',
-  },
-  {
-    efficiency: '100%',
-    lastRound: '1378',
-    qtyRounds: '120',
-    rank: '4',
     rewardCut: '75%',
-    roiETH: '0.0821660',
-    roiPer100LPT: '3.2176',
-    status: 'inactive',
-    totalStake: '42184.75',
-    transcoder: '0x1234...7890',
-    trend: 'down',
-  },
-  {
     efficiency: '100%',
-    lastRound: '1378',
-    qtyRounds: '120',
-    rank: '5',
-    rewardCut: '75%',
-    roiETH: '0.0821660',
+    activeRounds: '120',
     roiPer100LPT: '3.2176',
-    status: 'active',
-    totalStake: '42184.75',
-    transcoder: '0x1234...7890',
-    trend: 'up',
-  },
-  {
-    efficiency: '100%',
-    lastRound: '1378',
-    qtyRounds: '120',
-    rank: '6',
-    rewardCut: '75%',
-    roiETH: '0.0821660',
-    roiPer100LPT: '3.2176',
-    status: 'inactive',
-    totalStake: '42184.75',
-    transcoder: '0x1234...7890',
-    trend: 'down',
-  },
-  {
-    efficiency: '100%',
-    lastRound: '1378',
-    qtyRounds: '120',
-    rank: '7',
-    rewardCut: '75%',
-    roiETH: '0.0821660',
-    roiPer100LPT: '3.2176',
-    status: 'active',
-    totalStake: '42184.75',
-    transcoder: '0x1234...7890',
-    trend: 'up',
-  },
-  {
-    efficiency: '100%',
-    lastRound: '1378',
-    qtyRounds: '120',
-    rank: '8',
-    rewardCut: '75%',
-    roiETH: '0.0821660',
-    roiPer100LPT: '3.2176',
-    status: 'inactive',
-    totalStake: '42184.75',
-    transcoder: '0x1234...7890',
-    trend: 'down',
-  },
-  {
-    efficiency: '100%',
-    lastRound: '1378',
-    qtyRounds: '120',
-    rank: '9',
-    rewardCut: '75%',
-    roiETH: '0.0821660',
-    roiPer100LPT: '3.2176',
-    status: 'active',
-    totalStake: '42184.75',
-    transcoder: '0x1234...7890',
-    trend: 'up',
-  },
-  {
-    efficiency: '100%',
-    lastRound: '1378',
-    qtyRounds: '120',
-    rank: '10',
-    rewardCut: '75%',
-    roiETH: '0.0821660',
-    roiPer100LPT: '3.2176',
-    status: 'inactive',
-    totalStake: '42184.75',
-    transcoder: '0x1234...7890',
     trend: 'down',
   },
 ]
@@ -220,15 +111,17 @@ export class ROIRanking extends Component {
     const tableHead = (
       <TR>
         <THStyled textAlign="center">#</THStyled>
-        <THStyled>Transcoder</THStyled>
-        <THStyled textAlign="right">Total Stake</THStyled>
-        <THStyled textAlign="right">Reward Cut</THStyled>
-        <THStyled textAlign="right">Efficiency</THStyled>
+        <THStyled>Delegate address</THStyled>
+        <THStyled textAlign="right">Total stake</THStyled>
+        <THStyled textAlign="right">Reward cut</THStyled>
+        <THStyled textAlign="right" title={'Percentage of successful Reward Calls.'}>
+          Efficiency
+        </THStyled>
+        <THStyled textAlign="right">Active rounds</THStyled>
         <THStyled textAlign="right">ROI Per 1000 LPT</THStyled>
-        <THStyled textAlign="right">ROI (ETH)</THStyled>
-        <THStyled textAlign="right">QTY Rounds</THStyled>
-        <THStyled textAlign="right">Last Round</THStyled>
-        <THStyled textAlign="center">Trend</THStyled>
+        <THStyled textAlign="center" title={'Rewards trend of 30 days.'}>
+          Trend
+        </THStyled>
       </TR>
     )
 
@@ -241,23 +134,18 @@ export class ROIRanking extends Component {
               <TDStyled>
                 <TranscoderWrapper>
                   <Status status={item.status} />
-                  <ABlurred rel="noopener noreferrer" href={`#${item.transcoder}`} target="_blank">
-                    {item.transcoder}
-                  </ABlurred>
+                  <ABlurred>{item.address}</ABlurred>
                 </TranscoderWrapper>
               </TDStyled>
               <TDStyled textAlign="right">{item.totalStake}</TDStyled>
               <TDStyled textAlign="right">{item.rewardCut}</TDStyled>
               <TDStyled textAlign="right">{item.efficiency}</TDStyled>
+              <TDStyled textAlign="right">{item.activeRounds}</TDStyled>
               <TDStyled textAlign="right">{item.roiPer100LPT}</TDStyled>
-              <TDStyled textAlign="right">{item.roiETH}</TDStyled>
-              <TDStyled textAlign="right">{item.qtyRounds}</TDStyled>
-              <TDStyled textAlign="right">{item.lastRound}</TDStyled>
               <TDStyled textAlign="center">{item.trend === 'up' ? <ArrowUp /> : <ArrowDown />}</TDStyled>
             </TR>
           ))}
         </TableStyled>
-        <Pagination>{/* <SchemaLink onClick={this.showSchema}>Schema</SchemaLink> */}</Pagination>
       </ROICard>
     )
 
